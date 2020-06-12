d0beaeac96509991f01e22f2fcdb190a1e252b55
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-06-12 15:18:59 +0700

    Add files via upload

---
+++ Virusv1.sh
@@ -0,0 +1,153 @@
+#!/bin/Bash
+#dah lah gausah recode
+#tools bye cyber limit
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+
+sleep 2
+clear
+
+read -p "TADI SIAPA NAMA LU CUY? : " pil
+
+clear
+echo "TAMPILAN SEDERHANA!"
+echo "№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№"
+echo "-------Autor : Cyber Limit---------"
+echo "-----My team : Wisnu | Panjull-----"
+echo "№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№"
+echo "Note : Hargai Yang Membuat"
+echo ""
+echo ""
+read -p "MASUKAN NOMOR TARGET : " pil
+echo "PLEASE WAIT..."
+sleep 5
+echo -e "$red VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS BERHASIL DIKIRIM"
+sleep 1
+echo "VIRUS SELESAI DILAKUKAN✓"
