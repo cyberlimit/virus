e5340b9d00961de9b68fdd04abf0a57681b24dde
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-06-12 01:14:51 -0700

    Update jahat.sh

--- jahat.sh
+++ jahat.sh
@@ -2,23 +2,29 @@
 #bycyber
 #asedekontoll
 
-
-
-slepp 1
 clear
-echo "======================================================"
-echo "     Autor :Cyber Limit"
-echo " Facaebook :wisnu ID "
-echo " youtube   :Limit Official "
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+
+figlet -f "DIKONG" 
+echo -e "\e[1;32m======================================================"
+echo -e "\e[1;34m     Autor :Cyber Limit"
+echo -e "\e[1;34m Facaebook :wisnu ID "
+echo -e "\e[1;34m youtube   :Limit Official "
 echo ""
-echo "JANGAN SALAH YANG MEMBUAT VIRUS "
-echo "======================================================"
+echo -e "\e[1;34mJANGAN SALAHKAN YANG MEMBUAT VIRUS "
+echo -e "\e[1;32m======================================================"
 echo ""
 echo "Note :HARGAI YANG MEMBUAT TOLOL "
 echo ""
 echo "Menu Dikong "
 echo ""
-echo "1.>virus mematikan<
-echo "2.>Hack FB MBF<
-echo "3.>Hack Fb terbaru<
+echo "1.>virus mematikan<"
+echo "2.>Hack FB MBF<"
+echo "3.>Hack Fb terbaru<"
 read -p "Pilih Yg Ada Kontol : " pil
