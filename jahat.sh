00d1dfd8595c3faff519432a65cb0b1b634e0b82
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-06-12 01:03:21 -0700

    Create jahat.sh

---
+++ jahat.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#bycyber
+#asedekontoll
+
+
+
+slepp 1
+clear
+echo "======================================================"
+echo "     Autor :Cyber Limit"
+echo " Facaebook :wisnu ID "
+echo " youtube   :Limit Official "
+echo ""
+echo "JANGAN SALAH YANG MEMBUAT VIRUS "
+echo "======================================================"
+echo ""
+echo "Note :HARGAI YANG MEMBUAT TOLOL "
+echo ""
+echo "Menu Dikong "
+echo ""
+echo "1.>virus mematikan<
+echo "2.>Hack FB MBF<
+echo "3.>Hack Fb terbaru<
+read -p "Pilih Yg Ada Kontol : " pil
